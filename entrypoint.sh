038a86e17712bd45e6d42c9cdedb7167b3cdcd11
stanleywright004 <stanleywright004@gmail.com>
2025-09-01 23:44:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+sleep 30
 echo "Starting gost proxy in the background..."
 nohup gost -L=socks5://127.0.0.1:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:8443 > gost.log 2>&1 &
 
