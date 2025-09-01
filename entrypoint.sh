c24028a08f99c18d65e3c4920435be28195e25da
stanleywright004 <stanleywright004@gmail.com>
2025-09-01 21:04:59 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-gost -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:8443
+gost -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:8443 &
 
 sleep 2
 
